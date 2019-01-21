a829f8baf1bf1b28e46fada76ede324a611543f5
simon <swang@seven.com>
2019-01-21 14:47:06 +0800

    add c++ excetion code

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,18 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+
+using namespace std;
+
+class myexception: public exception
+{
+    virtual const char* what() const throw()
+    {
+        return "My exception happened";
+    }
+} myex;
+
+
 
 extern "C"
 JNIEXPORT jstring JNICALL
@@ -7,7 +20,16 @@ Java_com_gpxblog_exampleasan_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
-    int a[2] = {1, 0};
-    int b=a[2];
+    char *memory_leak = (char *)malloc(1000);
+    try
+    {
+        throw myex;
+    }
+    catch (exception& e)
+    {
+        __android_log_print(ANDROID_LOG_INFO,"test","get exeption:%s",e.what());
+    }
+
+
     return env->NewStringUTF(hello.c_str());
 }
